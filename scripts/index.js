56ee18ce4781557912151fd35f8ed63d3024bec5
MariaRez <maria.v.reznik@yandex.ru>
2022-06-26 18:07:15 +0300

    Оформлена функция открытия попапа через стрелочную, добавлен попап для открытия картинки

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,16 @@
 const editPopupButton = document.querySelector('.profile__edit-bottom');
 const closePopupButton = document.querySelector('.popup__close-botton_place_profile');
-const popup = document.querySelector('.popup_place_profile');
+const popupProfile = document.querySelector('.popup_place_profile');
 const formElement = document.querySelector('.form_place_profile');
 const profileName = document.querySelector('.profile__name'); 
 const profileDescription = document.querySelector('.profile__description');
 const profileNewName = document.querySelector('#name');
 const profileNewDescription = document.querySelector('#description');
-const popupCard = document.querySelector('.popup_place_card');
-const addPopupCardButton = document.querySelector('.profile__add-bottom');
-const closePopupCardButton = document.querySelector('.popup__close-botton_place_card');
-
-const cards = document.querySelector('.elements');
-const cardTemplate = document.querySelector('.elements-template').content;
+const popupCard = document.querySelector('.popup_place_card'); //попап для добавления новой карточки
+const addPopupCardButton = document.querySelector('.profile__add-bottom'); //кнопка добавления новой карточки
+const closePopupCardButton = document.querySelector('.popup__close-botton_place_card'); //кнопка закрытия попапа с новой карточкой
+const cards = document.querySelector('.elements'); // константа для добавления разметки карточки
+const cardTemplate = document.querySelector('.elements-template').content; // константа для добавления разметки карточки
 const initialCards = [
     {
       name: 'Архыз',
@@ -37,43 +36,37 @@ const initialCards = [
       name: 'Байкал',
       link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     }
-  ]; 
+  ]; //изначальньные карточки на странице 
+
   initialCards.forEach(function (element) {
     const cardElement = cardTemplate.cloneNode(true);
-  
     cardElement.querySelector('.element__text').textContent = element.name;
     cardElement.querySelector('.element__image').src = element.link;
-  
-    cards.append(cardElement)
+    cardElement.querySelector('.popup__text').textContent = element.name;// подпись картинки в попап ???
+    cardElement.querySelector('.popup__image').src = element.link;// картинка в попап ???
+    cardElement.querySelector('.element__like').addEventListener('click',function(evt){
+        evt.target.classList.toggle('element__like_active'); //реализована возможность ставить и убирать лайки
+    })
+    const popupImage =  cardElement.querySelector('.popup_place_image'); //нашли картинку в конкретной карточке
+    cardElement.querySelector('.element__image').addEventListener('click', () => toggleModalWindow(popupImage));
+    cards.append(cardElement);
 }) // Шесть карточек «из коробки»
-  
-function openPopup() {
+
+const toggleModalWindow = (popup) => {
+    popup.classList.toggle('popup_opened');
     profileNewName.value = profileName.textContent;
     profileNewDescription.value = profileDescription.textContent;
-    popup.classList.add('popup_opened');
-};
-
-function closePopup() {
-    popup.classList.remove('popup_opened');
-};
+}
 
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
     profileName.textContent = profileNewName.value;
     profileDescription.textContent = profileNewDescription.value;
-    popup.classList.remove('popup_opened');
+    popupProfile.classList.remove('popup_opened');
 };
 
-function openCardPopup() {
-    popupCard.classList.add('popup_opened');
-};
-
-function closeCardPopup() {
-    popupCard.classList.remove('popup_opened');
-};
-
-editPopupButton.addEventListener('click', openPopup);
-closePopupButton.addEventListener('click', closePopup);
+editPopupButton.addEventListener('click', () => toggleModalWindow(popupProfile));
+closePopupButton.addEventListener('click', () => toggleModalWindow(popupProfile));
 formElement.addEventListener('submit', formSubmitHandler); 
-addPopupCardButton.addEventListener('click', openCardPopup);
-closePopupCardButton.addEventListener('click',closeCardPopup);
+addPopupCardButton.addEventListener('click', () => toggleModalWindow(popupCard));
+closePopupCardButton.addEventListener('click', () => toggleModalWindow(popupCard));
