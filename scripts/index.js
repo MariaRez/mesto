d6027f3e3fdbd5130098faae07362c280958dd47
MariaRez <maria.v.reznik@yandex.ru>
2022-07-09 21:56:57 +0300

    Реализована валидация нового места и профиля пользователя

--- scripts/index.js
+++ scripts/index.js
@@ -35,11 +35,11 @@ formElement.addEventListener('submit', submitHandlerFormProfile);
 popupCardButtonAdd.addEventListener('click', () => openModalWindow(popupCard));
 popupCardButtonClose.addEventListener('click', () => closeModalWindow(popupCard));
 
-/* новый функционал */
+/* вторая часть функционала */
 
 const initialCardsTemplate = document.querySelector('.elements-template').content;
 const elements = document.querySelector('.elements');
-const formButton = document.querySelector('.popup__field-button_place_card');
+const formButton = document.querySelector('.popup__button_place_card');
 const formInputName = document.querySelector('.popup__field_type_placename');
 const formInputLink = document.querySelector('.popup__field_type_placelink');
 
@@ -115,3 +115,69 @@ function openPopupImage (evt) {
 
 /*закрытие попапа с картинкой при нажатии на крестик*/
 popupImageButtonClose.addEventListener('click', () => closeModalWindow(popupImage));
+
+
+/*третий функционал*/
+/*для профиля - аналогично необходимо сделать для места*/
+formElement.addEventListener('submit', sendForm);
+formElement.addEventListener('input', handlerInputForm); //для события инпута ввода данных в соотвествующей форме 
+ 
+function sendForm (evt) {
+  evt.preventDefault();
+  const form = evt.target;
+  if (form.checkValidity()) {
+    console.log('Форма валидна') //здесь потом должна быть логика отправки данных на сервер
+  } else {
+    console.log('Форма не валидна')
+  }
+}
+
+function handlerInputForm(evt){ //функция обработчик  инпута 
+const curentForm = evt.currentTarget; // тот объект для которого мы выбрали подписку 
+validateForm(curentForm);
+validateInput(evt.target);
+}
+
+function validateForm(form) {
+  const submitButton = form.querySelector('.popup__button')//находим кнопку(класс выбран для обеих кнопок - но сама по себе функция опирается на конкретную форму) для того чтобы делать ее активной или неактивной
+  if (form.checkValidity()) { //если форма валидна, то для кнопки 
+    submitButton.removeAttribute('disabled'); //удаляем атрибут у кнопки тем самым делаем ее активной
+    submitButton.classList.add('popup__button_active'); //такого класса у меня нет - он отвечает за стиль когда кнопка должна быть активна - по заданию другой
+    submitButton.classList.remove('popup__button_disabled'); //такого класса у меня нет - он отвечает за стиль когда кнопка должна быть неактивна - по заданию другой
+     } else { // если форма не валидна (проверки не пройдены)
+    submitButton.setAttribute('disabled',true); //добавить кнопку атрибут блокирующий доступ
+    submitButton.classList.remove('popup__button_active');
+    submitButton.classList.add('popup__button_disabled');
+     }
+} 
+
+function validateInput (input){
+   addCustomErrorMessage (input); //по заданию должны быть браузерные ошибки по умолчанию - необходимо удалить данную функцию
+   const errorElement = input.parentNode.querySelector(`#${input.id}-error`); //находим элемент в который будем выводить сообщения об ошибке через айди инпута и спана
+   errorElement.textContent = input.validationMessage;//текстовый контент внутри тега span по умолчанию от браузера
+  }
+
+function addCustomErrorMessage(input){ //функция для ошибок в инпут 
+  input.setCustomValidity(''); //свойство передающее validationMessage то есть текстовый контент внутри тега span - ошибки 
+  if (input.validity.valueMissing) { //все свойства можно узнать вывев в консоль input.validity в самом начале функции - для каждого случая описываем свою ситуацию: когда ничего не написано, когда короткая и тд
+    input.setCustomValidity('Вы пропустили это поле.');
+  }
+  if ((input.validity.tooShort || input.validity.tooLong) && input.name === 'Name') { // для имени пользователя
+    input.setCustomValidity('Должно быть от 2 до 40 символов.');
+  }
+  if ((input.validity.tooShort || input.validity.tooLong) && input.name === 'about') { // для описания
+    input.setCustomValidity('Должно быть от 2 до 200 символов.');
+  }
+  if ((input.validity.tooShort || input.validity.tooLong) && input.name === 'name-card') { // для названия карточки
+    input.setCustomValidity('Должно быть от 2 до 30 символов.');
+  }
+  if (input.validity.typeMismatch && input.type === 'url') {
+    input.setCustomValidity('Должно быть ссылкой.');
+  } //для url
+}
+
+/*функционал по вебинару второй формы*/
+formCard.addEventListener('submit', sendForm);
+formCard.addEventListener('input', handlerInputForm);
+
+validateForm(formCard); //для проверки в самом начале - для этой формы обязательно
