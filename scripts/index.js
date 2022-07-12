1dce7bca44f3565fe2a2bd18d9da8bf0117cd243
MariaRez <maria.v.reznik@yandex.ru>
2022-07-12 19:35:42 +0300

    Переделан функционал валидации форм, переделан функционал закрытия попапа по нажатию на esc и overlat

--- scripts/index.js
+++ scripts/index.js
@@ -12,10 +12,12 @@ const popupCardButtonClose = document.querySelector('.popup__close-button_place_
 
 const openModalWindow = (popup) => {
   popup.classList.add('popup_opened');
+  document.addEventListener('keydown', closeEsc);// добавляем слушатель при открытом попапе - при нажатии закрываем попап
 }
 
 const closeModalWindow = (popup) => {
   popup.classList.remove('popup_opened');
+  document.removeEventListener('keydown', closeEsc);// убираем слушатель
 }
 
 function submitHandlerFormProfile (evt) {
@@ -122,108 +124,21 @@ function openPopupImage (evt) {
 popupImageButtonClose.addEventListener('click', () => closeModalWindow(popupImage));
 
 
-/*третий функционал*/
-/*для профиля - аналогично необходимо сделать для места*/
-formElement.addEventListener('submit', sendForm);
-formElement.addEventListener('input', handlerInputForm); //для события инпута ввода данных в соотвествующей форме 
- 
-function sendForm (evt) {
-  evt.preventDefault();
-  const form = evt.target;
-  if (form.checkValidity()) {
-    console.log('Форма валидна') //здесь потом должна быть логика отправки данных на сервер
-  } else {
-    console.log('Форма не валидна')
-  }
-}
-
-function handlerInputForm(evt){ //функция обработчик  инпута 
-const curentForm = evt.currentTarget; // тот объект для которого мы выбрали подписку 
-validateForm(curentForm);
-validateInput(evt.target);
-}
-
-function validateForm(form) {
-  const submitButton = form.querySelector('.popup__button')//находим кнопку(класс выбран для обеих кнопок - но сама по себе функция опирается на конкретную форму) для того чтобы делать ее активной или неактивной
-  if (form.checkValidity()) { //если форма валидна, то для кнопки 
-    submitButton.removeAttribute('disabled'); //удаляем атрибут у кнопки тем самым делаем ее активной
-    submitButton.classList.add('popup__button_active'); //такого класса у меня нет - он отвечает за стиль когда кнопка должна быть активна - по заданию другой
-    submitButton.classList.remove('popup__button_disabled'); //такого класса у меня нет - он отвечает за стиль когда кнопка должна быть неактивна - по заданию другой
-     } else { // если форма не валидна (проверки не пройдены)
-    submitButton.setAttribute('disabled',true); //добавить кнопку атрибут блокирующий доступ
-    submitButton.classList.remove('popup__button_active');
-    submitButton.classList.add('popup__button_disabled');
-     }
-} 
-
-function validateInput (input){
-   addCustomErrorMessage (input); //по заданию должны быть браузерные ошибки по умолчанию - необходимо удалить данную функцию
-   const errorElement = input.parentNode.querySelector(`#${input.id}-error`); //находим элемент в который будем выводить сообщения об ошибке через айди инпута и спана
-   errorElement.textContent = input.validationMessage;//текстовый контент внутри тега span по умолчанию от браузера
-  }
-
-function addCustomErrorMessage(input){ //функция для ошибок в инпут 
-  input.setCustomValidity(''); //свойство передающее validationMessage то есть текстовый контент внутри тега span - ошибки 
-  if (input.validity.valueMissing) { //все свойства можно узнать вывев в консоль input.validity в самом начале функции - для каждого случая описываем свою ситуацию: когда ничего не написано, когда короткая и тд
-    input.setCustomValidity('Вы пропустили это поле.');
-  }
-  if ((input.validity.tooShort || input.validity.tooLong) && input.name === 'Name') { // для имени пользователя
-    input.setCustomValidity('Должно быть от 2 до 40 символов.');
-  }
-  if ((input.validity.tooShort || input.validity.tooLong) && input.name === 'about') { // для описания
-    input.setCustomValidity('Должно быть от 2 до 200 символов.');
-  }
-  if ((input.validity.tooShort || input.validity.tooLong) && input.name === 'name-card') { // для названия карточки
-    input.setCustomValidity('Должно быть от 2 до 30 символов.');
-  }
-  if (input.validity.typeMismatch && input.type === 'url') {
-    input.setCustomValidity('Должно быть ссылкой.');
-  } //для url
-}
-
-/*функционал по вебинару второй формы*/
-formCard.addEventListener('submit', sendForm);
-formCard.addEventListener('input', handlerInputForm);
-
-validateForm(formCard); //для проверки в самом начале - для этой формы обязательно
-
 // закрытие попапов по нажатию на overlay
-popupProfile.addEventListener('click', (event) => {
-  if(event.target == event.currentTarget) {
-closeModalWindow(popupProfile);
-  }
+const popups = Array.from(document.querySelectorAll('.popup')); //берем массив попапов
+
+popups.forEach((popupElement) => {
+  popupElement.addEventListener('mousedown', function(evt) {
+    if (evt.target === evt.currentTarget) {
+      closeModalWindow(popupElement);
+    }
+  });
 });
 
-popupCard.addEventListener('click', (event) => {
-  if(event.target == event.currentTarget) {
-closeModalWindow(popupCard);
-  }
-});
-
-popupImage.addEventListener('click', (event) => {
-  if(event.target == event.currentTarget) {
-closeModalWindow(popupImage);
-  }
-});
-
-// закрытие попапов по нажатию на Esc - перепроверить результат
-popupProfile.addEventListener('keypress',(event) => {
-  if(event.keyCode === 'Escape') {
-    event.preventDefault();
-    closeModalWindow(popupProfile);
-  }
-});
-
-popupCard.addEventListener('keypress',(event) => {
-  if(event.keyCode === 'Escape') {
-    event.preventDefault();
-    closeModalWindow(popupCard);
-  }
-});
-
-popupImage.addEventListener('keypress',(event) => {
-  if(event.keyCode === 'Escape') {
-    event.preventDefault();
-    closeModalWindow(popupImage);
-  }
-});
+//закрытие попапов при нажатии на esc
+function closeEsc(evt) {
+  if (evt.key === "Escape"){
+  const activePopup = document.querySelector('.popup_opened');
+  closeModalWindow(activePopup);
+}
+};
---
+++ scripts/validate.js
@@ -0,0 +1,72 @@
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.add('popup__field_type_error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('error_active');
+};
+
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.remove('popup__field_type_error');
+  errorElement.classList.remove('error_active');
+  errorElement.textContent = '';
+};
+
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+const toggleButtonState = (inputList, buttonElement) => {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.remove('popup__button_active');
+    buttonElement.classList.add('popup__button_disabled');
+    buttonElement.setAttribute('disabled', true)
+  } else {
+    buttonElement.classList.add('popup__button_active');
+    buttonElement.classList.remove('popup__button_disabled');
+    buttonElement.removeAttribute('disabled')
+  }
+};
+
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  })
+};
+
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll('.form__input'));
+  const buttonElement = formElement.querySelector('.popup__button');
+  toggleButtonState(inputList, buttonElement);
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', function () {
+      checkInputValidity(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
+
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll('.form'));
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', function (evt) {
+      evt.preventDefault();
+    });
+    setEventListeners(formElement)
+  });
+}
+
+validationSettings ={
+  formElement: '.form',
+  inputElement: '.popup__field',
+  buttonElement: '.popup__button',
+  inactiveButtonClass: '.popup__button_disabled',
+  inputErrorClass: '.popup__field_type_error',
+  errorElement: '.error'
+}
+
+enableValidation(validationSettings);
