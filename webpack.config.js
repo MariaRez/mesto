e5fffe76ca8345f32b70f9f630c44b13230340eb
MariaRez <maria.v.reznik@yandex.ru>
2022-08-19 16:01:38 +0300

    Перенастроили webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
-const path = require('path'); // подключаем path к конфигу вебпак
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // подключили плагин 
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 module.exports = {
   entry: { main: './src/pages/index.js' },
   output: {
@@ -8,32 +9,26 @@ module.exports = {
     filename: 'main.js',
         publicPath: ''
   },
-  mode: 'development', // добавили режим разработчика
+    mode: 'development',
   devServer: {
-    static: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
-    compress: true, // это ускорит загрузку в режиме разработки
-    port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
-
-    open: true // сайт будет открываться сам при запуске npm run dev
+    static: path.resolve(__dirname, './dist'),
+    compress: true,
+    port: 8080,
+    open: true
   },
   module: {
-    /*«если тебе попадётся файл с расширением .js, сначала отдай этот файл модулю babel-loader, а затем добавляй в сборку. Но не применяй это правило к пакетам, скачанным из NPM, которые лежат в папке node_modules».*/
-    rules: [ // rules — это массив правил
-      // добавим в него объект правил для бабеля
+    rules: [
       {
-        // регулярное выражение, которое ищет все js файлы
         test: /\.js$/,
-        // при обработке этих файлов нужно использовать babel-loader
         use: 'babel-loader',
-        // исключает папку node_modules, файлы в ней обрабатывать не нужно
-        exclude: '/node_modules/'
+        exclude: /node_modules/
       }
-      ]
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html'
     }),
-    new CleanWebpackPlugin(), // использовали плагин
-  ] 
-}
+        new CleanWebpackPlugin(),
+  ]
+};
